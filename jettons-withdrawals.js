9f20f7104411771793dfbbdf07f0ca4860f12de2
tolya-yanot <your_email@abc.example>
2023-03-01 17:01:53 +0300

    add checkJettonTransfer

--- jettons-withdrawals.js
+++ jettons-withdrawals.js
@@ -68,10 +68,11 @@ const init = async () => {
 
         await wallet.methods.transfer({
             secretKey: keyPair.secretKey,
-            toAddress: jettons[jettonName].jettonWalletAddress,
+            toAddress: jettons[jettonName].jettonWalletAddress.toString(true, true, true),
             amount: TonWeb.utils.toNano('0.05'), // TON
             seqno: seqno,
             payload: await jettonWallet.createTransferBody({
+                queryId: seqno, // any number
                 jettonAmount: jettonAmountInUnits,
                 toAddress: new TonWeb.utils.Address(destinationAddress),
                 responseAddress: hotWalletAddress
@@ -80,7 +81,51 @@ const init = async () => {
         }).send()
     }
 
-    await jettonTransfer('KOTE', 'EQA0i8-CdGnF_DhUHHf92R1ONH6sIA9vLZ_WLcCIhfBBXwtG', new TonWeb.utils.BN('1'))
+    // check that jetton transfer with specified `queryId` successfully processed
+    const checkJettonTransfer = async (jettonName, transferQueryId) => {
+        const jettonWalletAddress = jettons[jettonName].jettonWalletAddress.toString(false);
+
+        const transactions = await tonweb.provider.getTransactions(jettonWalletAddress);
+        console.log('Check last ' + transactions.length + ' transactions');
+
+        for (const tx of transactions) {
+            const sourceAddress = tx.in_msg.source;
+            if (new TonWeb.utils.Address(sourceAddress).toString(false) !== hotWalletAddress.toString(false)) {
+                continue;
+            }
+
+            if (!tx.in_msg.msg_data ||
+                tx.in_msg.msg_data['@type'] !== 'msg.dataRaw' ||
+                !tx.in_msg.msg_data.body
+            ) {
+                // no in_msg or in_msg body
+                continue;
+            }
+
+            const msgBody = TonWeb.utils.base64ToBytes(tx.in_msg.msg_data.body);
+
+            const cell = TonWeb.boc.Cell.oneFromBoc(msgBody);
+            const slice = cell.beginParse();
+            const op = slice.loadUint(32);
+            if (!op.eq(new TonWeb.utils.BN(0x0f8a7ea5))) continue; // op == transfer
+            const queryId = slice.loadUint(64);
+            const amount = slice.loadCoins();
+            const destinationAddress = slice.loadAddress();
+            if (queryId.eq(new TonWeb.utils.BN(transferQueryId))) {
+                if (tx.out_msgs.length === 0) {
+                    return false; // Error in jetton transfer - no out messages produced
+                }
+                if (tx.out_msgs.length === 1 && new TonWeb.utils.Address(tx.out_msgs[0].destination).toString(false) === hotWalletAddress.toString(false)) {
+                    return false; // Error in jetton transfer - bounced message
+                }
+                return true; // successful jetton transfer
+            }
+        }
+        return false;
+    }
+
+    await jettonTransfer('KOTE', 'EQA0i8-CdGnF_DhUHHf92R1ONH6sIA9vLZ_WLcCIhfBBXwtG', new TonWeb.utils.toNano('1'))
+    // console.log(await checkJettonTransfer('KOTE', 0)); // note that you need to wait until the transaction is processed, do not call immediately
 }
 
 init();
